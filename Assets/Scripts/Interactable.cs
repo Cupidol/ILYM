e09231de60557e96f306afdc512a0c410f8cea6e
Valentijn Dol <valentijndol@gmail.com>
2018-06-14 18:00:32 +0200

    Added Interactable.cs

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour{
+	public door door;
+    Message component;
+    bool isPlayed;
+	public bool isExit;
+
+    private void Start()
+    {
+        component = GetComponent<Message>();
+  
+    }
+    void Update () {
+		
+	}
+	public void ActivateObject(){
+		//door.Toggle();
+		if (!isExit && door != null) {
+			door.Toggle ();
+		} else if (door != null) {
+			component.invoke();
+		}
+        Debug.Log("Ik word aangeroepen");
+		if (component != null && !isPlayed){
+            isPlayed = true;
+            component.invoke();
+        }
+    }
+}
