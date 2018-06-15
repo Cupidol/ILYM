b0deb272cba4fc1741bee742005cda813105b41a
Valentijn Dol <valentijndol@gmail.com>
2018-06-15 18:22:41 +0200

    Added more interaction

--- Assets/Scripts/StoryInteractable.cs
+++ Assets/Scripts/StoryInteractable.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class StoryInteractable : MonoBehaviour {
 
     public bool isUsed;
+   // public SpriteRenderer indicatorSpriteRenderer;
+    public GameObject indicator;
 
+    public void Start()
+    {
+       // indicatorSpriteRenderer = GetComponent<SpriteRenderer>();
+    } 
     public void AddToStoryList() {
         if (!StoryManager.Instance.storyList.Contains(this)) {
                  StoryManager.Instance.storyList.Add(this);
             }
-
+        isUsed = true;
+        //indicatorSpriteRenderer.enabled = false;
+        indicator.GetComponent<SpriteRenderer>().enabled = false;
     }
 
 }
